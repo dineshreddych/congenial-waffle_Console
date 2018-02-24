a161ccaf67bcd2de12d0db46fa2c9978867c69ae
dineshreddych <dineshreddy.cheedu@gmail.com>
2018-02-23 23:00:22 -0500

    Created console app

---
+++ AlgorithmsFunConsole/AlgorithmsFunConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmsFunConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
